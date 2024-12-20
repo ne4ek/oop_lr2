d921b0a6d8f6ef358f39f6f00c8d15c3f9588e27
ne4ek <nechekpojiloy@gmail.com>
2024-12-20 17:00:56 +0300

    add main fun

--- main.py
+++ main.py
@@ -310,6 +310,71 @@ def drawGameTime(gameTime):
     screen.blit(game_time_numb, (105, 14))
 
 
-def exitScreen():
-    pass
-      
+def main():
+    score = 0
+
+    mySnake = Snake(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    mySnake.setDirection(KEY["UP"])
+    mySnake.move()
+    start_segments = 3
+    while start_segments > 0:
+        mySnake.grow()
+        mySnake.move()
+        start_segments -= 1
+
+    max_apples = 1
+    eaten_apple = False
+    apples = [
+        Apple(random.randint(60, SCREEN_WIDTH), random.randint(60, SCREEN_HEIGHT), 1)
+    ]
+    respawnApples(apples, max_apples, mySnake.x, mySnake.y)
+
+    startTime = pygame.time.get_ticks()
+    endgame = 0
+
+    while endgame != 1:
+        gameClock.tick(FPS)
+
+        keyPress = getKey()
+        if keyPress == "exit":
+            endgame = 1
+
+        checkLimits(mySnake)
+        if mySnake.checkCrash() == True:
+            endGame()
+
+        for myApple in apples:
+            if myApple.state == 1:
+                if (
+                    checkCollision(mySnake.getHead(), SNAKE_SIZE, myApple, APPLE_SIZE)
+                    == True
+                ):
+                    mySnake.grow()
+                    myApple.state = 0
+                    score += 5
+                    eaten_apple = True
+
+        if keyPress:
+            mySnake.setDirection(keyPress)
+        mySnake.move()
+
+        if eaten_apple == True:
+            eaten_apple = False
+            respawnApple(apples, 0, mySnake.getHead().x, mySnake.getHead().y)
+
+        screen.fill(background_color)
+        for myApple in apples:
+            if myApple.state == 1:
+                myApple.draw(screen)
+        mySnake.draw(screen)
+        drawScore(score)
+        gameTime = pygame.time.get_ticks() - startTime
+        drawGameTime(gameTime)
+
+        pygame.display.flip()
+        pygame.display.update()
+
+
+
+if __name__ == "__main__":      
+    main()
